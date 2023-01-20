fcf07b6b1beb274907b6c6bd0b93091f8987f783
Cody Fackrell <codyfackrell56@gmail.com>
2023-01-19 21:56:10 -0700

    assessment part 1 completed.

--- assessment-js-basics-1/sdlc.js
+++ assessment-js-basics-1/sdlc.js
@@ -5,12 +5,12 @@
     Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
 */
 
-// Implementation
-// Design
-// Maintain
-// Analysis of Requirements
-// Testing & Integration
 // Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
 
 
 //////////////////PROBLEM 2////////////////////
@@ -19,3 +19,8 @@
     The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const analysis = "This is the second step when you would determine the system and software requirments."
+const design = "This is the third step, in which, typically the UX team, would design the layout and funcationality of the application. In addtion to the layout of the app, this phase will also determine the visual design of the app"
+const implementation = "This is the fourth step where the software and web development code the application according to the requirments and desgin that had taken place previously."
+const testingIntegration = "This is the fifth step where the QA team will test the application to look for bugs that need addressed before it is released to the public."
+const maintain = "This is the sixith and ongoing step where changes may be may to the application and the teams continue to ensure proper functionality of the app."
