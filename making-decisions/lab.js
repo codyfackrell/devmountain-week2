abc3bfb5c8323951235a2f3b72a2e77a65c52615
Cody Fackrell <codyfackrell56@gmail.com>
2023-01-19 20:40:54 -0700

    problem 9 completed.

--- making-decisions/lab.js
+++ making-decisions/lab.js
@@ -131,7 +131,25 @@ while (score < passingScore) {
 
 */
 
-//CODE HERE
+let changeMyMind = false;
+
+if (changeMyMind === true) {
+  changeMyMind = false
+  console.log(changeMyMind)
+} else if (changeMyMind === false) {
+  changeMyMind = true
+  console.log(changeMyMind)
+}
+
+// OR
+
+if (changeMyMind) {
+  changeMyMind = false
+} else {
+  changeMyMind = true
+}
+
+// ** While working through this problem, found two ways to do it which are above.
 
 ////////// PROBLEM 10 //////////
 // Using the "not" operator (!), change the current value of changeMyMind to true (it should currently be set to false due to the if-else statement in Problem 9). After you change the value of changeMyMind, console.log it's new value (it should now read true).
