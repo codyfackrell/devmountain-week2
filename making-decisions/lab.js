1f4c63f5e7f0e368bfcc0f049c465f8d70c9e3e9
Cody Fackrell <codyfackrell56@gmail.com>
2023-01-19 20:05:54 -0700

    problem 5 completed.

--- making-decisions/lab.js
+++ making-decisions/lab.js
@@ -76,7 +76,9 @@ if (temperature >= 80 && rain === true) {
   Create a for-loop that runs 10 times, and on each iteration logs the word 'hello'.
 */
 
-//CODE HERE
+for (i = 0; i < 10; i++) { 
+  console.log("Hello!")
+}
 
 ////////// PROBLEM 6 //////////
 
